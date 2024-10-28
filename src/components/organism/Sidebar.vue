7365b87ab54dfe39a83d48d3586702c0cee189e4
amirkian007 <amircc1200@gmail.com>
2024-10-28 22:30:42 +0330

    feat: make sidebar responsive

--- src/components/organism/Sidebar.vue
+++ src/components/organism/Sidebar.vue
@@ -1,27 +1,42 @@
 <script lang="ts" setup>
 // components
+import { onMounted, onUnmounted, ref } from "vue";
 import SidebarItem from "../molecules/SidebarItem.vue";
 // props
 const props = withDefaults(defineProps<{ modelValue: boolean }>(), {
-  modelValue: false
+  modelValue: false,
 });
 //emits
 const emit = defineEmits<{
   (e: "update:modelValue", v: any): void;
 }>();
 //data
-const isResponsive = innerWidth < 992
+const isResponsive = ref(true);
 //methods
 function closeItem() {
   if (props.modelValue) {
-    emit('update:modelValue', false)
+    emit("update:modelValue", false);
   }
 }
+function windowResizerListner(){
+  isResponsive.value = innerWidth < 992;
+};
+//lifecycle
+onMounted(() => {
+  windowResizerListner()
+  window.addEventListener("resize", windowResizerListner);
+});
+onUnmounted(() => {
+  window.removeEventListener("resize", windowResizerListner);
+});
 </script>
 
 <template>
-  <nav class="sidebar bg-primary h-100 pt-1" v-click-outside="closeItem"
-    :class="{ 'responisve-mode': isResponsive, 'menu-open': props.modelValue }">
+  <nav
+    class="sidebar bg-primary h-100 pt-1"
+    v-click-outside="closeItem"
+    :class="{ 'responisve-mode': isResponsive, 'menu-open': props.modelValue }"
+  >
     <h4 class="m-3">Post</h4>
     <SidebarItem @click="closeItem" label="All Articles" to="/" />
     <SidebarItem @click="closeItem" label="New Article" to="/newArticle" />
