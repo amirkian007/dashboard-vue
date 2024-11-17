ea180cc728e8682463888df87283c2c2f7f279d8
amirkian007 <amircc1200@gmail.com>
2024-11-17 23:56:14 +0330

    feat: lazy load routes

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,74 +1,71 @@
 //utils
-import { createRouter, createWebHistory } from 'vue-router';
-import { useAuthStore } from '@/stores/auth';
+import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
 //componnets
-import Login from '@//pages/Login.vue';
-import Register from '@//pages/Register.vue';
-import NotFound from '@/pages/NotFound.vue';
-import DashboardTemplate from '@/components/templates/DashboardTemplate.vue';
-import DashBoard from '@//pages/DashBoard.vue';
-import NewArticle from '@/pages/NewArticle.vue';
-import EditArticle from '@/pages/EditArticle.vue';
+import Login from '@//pages/Login.vue'
+import Register from '@//pages/Register.vue'
+import NotFound from '@/pages/NotFound.vue'
+import DashboardTemplate from '@/components/templates/DashboardTemplate.vue'
+import DashBoard from '@//pages/DashBoard.vue'
+import NewArticle from '@/pages/NewArticle.vue'
+import EditArticle from '@/pages/EditArticle.vue'
 
 const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: Login,
+    component: () => import('@/pages/Login.vue'),
   },
   {
     path: '/register',
     name: 'Register',
-    component: Register,
+    component: () => import('@/pages/Register.vue'),
   },
   {
     path: '/',
-    component: DashboardTemplate,
+    component: () => import('@/components/templates/DashboardTemplate.vue'),
     meta: { requiresAuth: true },
-    children:[
+    children: [
       {
         path: '/',
         name: 'Dashboard',
-        component: DashBoard,
+        component: () => import('@//pages/DashBoard.vue'),
       },
       {
         path: '/newArticle',
         name: 'NewArticle',
-        component: NewArticle,
+        component: () => import('@/pages/NewArticle.vue'),
       },
       {
         path: '/editArticle/:slug',
         name: 'EditArticle',
-        component: EditArticle,
+        component: import('@/pages/EditArticle.vue'),
       },
-    ]
+    ],
   },
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
-    component: NotFound,
+    component: import('@/pages/NotFound.vue'),
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-});
+})
 
 router.beforeEach((to, from, next) => {
-
-  const authStore = useAuthStore();
-  const isAuthenticated = authStore.isAuthenticated;
+  const authStore = useAuthStore()
+  const isAuthenticated = authStore.isAuthenticated
 
   if ((to.name === 'Login' || to.name === 'Register') && isAuthenticated) {
-    next({ name: 'Dashboard' });
-  } 
-  else if (to.meta.requiresAuth && !isAuthenticated) {
-    next({ name: 'Login' });
+    next({ name: 'Dashboard' })
+  } else if (to.meta.requiresAuth && !isAuthenticated) {
+    next({ name: 'Login' })
   } else {
-    next();
+    next()
   }
+})
 
-}); 
-
-export default router;
+export default router
