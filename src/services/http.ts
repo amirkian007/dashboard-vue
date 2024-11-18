4f965cb4119a2439a60d466448a39c84f8c0b389
amirkian <amircc1200@gmail.com>
2024-11-18 08:28:11 +0330

    chore: Add todo comment fot status code handeling in https

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,95 +1,99 @@
 //utils
-import { API_URL } from "@/configs/global";
-import { useAuthStore } from "@/stores/auth";
+import { API_URL } from '@/configs/global'
+import { useAuthStore } from '@/stores/auth'
 import axios, {
   AxiosRequestConfig,
   AxiosRequestHeaders,
   AxiosResponse,
-} from "axios";
+} from 'axios'
 //hooks
-import { useToast } from "vue-toastification";
+import { useToast } from 'vue-toastification'
 
-const toast = useToast();
+const toast = useToast()
 
 const httpService = axios.create({
   baseURL: API_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
-});
+})
 
 httpService.interceptors.response.use(
-  (response) => {
-    return response;
+  response => {
+    return response
   },
-  (error) => {
-    toast.error(error?.message);
-  }
-);
+  error => {
+    // TODO: handel status codes with a callback map or ...
+    // 401 --> logout
+    // 500 --> toast
+    // ...
+    toast.error(error?.message)
+  },
+)
 httpService.interceptors.request.use(function (config) {
-  const authStore = useAuthStore();
-  const token = authStore.token;
-  config.headers.Authorization = `Token ${token}`;
-  return config;
-});
+  const authStore = useAuthStore()
+  const token = authStore.token
+  config.headers.Authorization = `Token ${token}`
+  return config
+})
 
 async function apiBase<T>(
   url: string,
-  options?: AxiosRequestConfig
+  options?: AxiosRequestConfig,
 ): Promise<T> {
-  const response: AxiosResponse = await httpService(url, options);
-  return response.data as T;
+  const response: AxiosResponse = await httpService(url, options)
+  return response.data as T
 }
 
 async function readData<T>(
   url: string,
   params?: Object,
-  headers?: AxiosRequestHeaders
+  headers?: AxiosRequestHeaders,
 ): Promise<T> {
   const options: AxiosRequestConfig = {
     headers,
-    method: "GET",
+    method: 'GET',
     params,
-  };
-  return await apiBase<T>(url, options);
+  }
+  return await apiBase<T>(url, options)
 }
 
 async function postData<T, D>(
   url: string,
   data: T,
-  headers?: AxiosRequestHeaders
+  headers?: AxiosRequestHeaders,
 ): Promise<D> {
   const options: AxiosRequestConfig = {
     headers: headers,
-    method: "POST",
+    method: 'POST',
     data: JSON.stringify(data),
-  };
-  return await apiBase<D>(url, options);
+  }
+  return await apiBase<D>(url, options)
 }
 
 async function updateData<T, D>(
   url: string,
   data: T,
-  headers?: AxiosRequestHeaders
+  headers?: AxiosRequestHeaders,
 ): Promise<D> {
   const options: AxiosRequestConfig = {
     headers: headers,
-    method: "PUT",
+    method: 'PUT',
     data: JSON.stringify(data),
-  };
-  return apiBase<D>(url, options);
+  }
+  return apiBase<D>(url, options)
 }
 
 async function deleteData(
   url: string,
-  headers?: AxiosRequestHeaders
+  headers?: AxiosRequestHeaders,
 ): Promise<void> {
   const options: AxiosRequestConfig = {
     headers: headers,
-    method: "DELETE",
-  };
-  return apiBase(url, options);
+    method: 'DELETE',
+  }
+  return apiBase(url, options)
 }
-const http = { postData, readData, updateData, deleteData };
+const http = { postData, readData, updateData, deleteData }
 
-export default http;
+export default http
